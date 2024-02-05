fedca784341a9c5c707d3ea6db7bd50b351159aa
nagesh-merva <officialnagesh.merva@gmail.com>
2024-02-05 23:40:24 +0530

    !

--- wsgi.py
+++ wsgi.py
@@ -2,3 +2,8 @@ from app import app
 
 if __name__ == '__main__':
     app.run(debug=True)
+else:
+    from gunicorn.app.wsgiapp import WSGIApplication
+    app = WSGIApplication()
+    app.load_wsgiapp = lambda: app
+    app.run()
